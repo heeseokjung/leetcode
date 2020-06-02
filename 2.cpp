4063d080dae6945584cca301b7dba777f4e90410
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-06-02 06:05:54 -0700

    2020/06/02

--- 2.cpp
+++ 2.cpp
@@ -11,6 +11,31 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode *ret = nullptr, *p = nullptr;
+        while(l1 || l2) {
+            int tmp = carry;
+            if(l1 != nullptr)
+                tmp += l1->val;
+            if(l2 != nullptr)
+                tmp += l2->val;
+            carry = tmp / 10;
+            tmp %= 10;
+            if(ret == nullptr) {
+                ret = new ListNode(tmp);
+                p = ret;
+            } else {
+                p->next = new ListNode(tmp);
+                p = p->next;
+            }
+            
+            l1 = (l1 != nullptr ? l1->next : l1);
+            l2 = (l2 != nullptr ? l2->next : l2);
+        }
         
+        if(carry != 0)
+            p->next = new ListNode(carry);
+        
+        return ret;
     }
 };
