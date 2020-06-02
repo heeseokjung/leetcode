152c2f041503de9f5956a21655a6137f4de7db04
heeseokjung <hsjeong.cs@gmail.com>
2020-06-02 00:46:17 -0700

    2020/06/02

---
+++ 1.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> m;
+        for(int i = 0; i < (int)nums.size(); ++i)
+            m.insert(make_pair(nums[i], i));
+
+        vector<int> ans;
+        for(int i = 0; i < (int)nums.size(); ++i) {
+            auto it = m.find(target - nums[i]);
+            if(it != m.end() && i != it->second) {
+                ans.push_back(i);
+                ans.push_back(it->second);
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
